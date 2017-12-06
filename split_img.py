958b9b8fff158a07f1b6b9d92f82d58c672f7ec9
physknoll <harrison.knoll@gmail.com>
2017-12-05 23:44:38 -0600

    split is working

--- split_img.py
+++ split_img.py
@@ -1,100 +1,97 @@
-from PIL import Image, ImageDraw
+from PIL import Image
 import xml.etree.ElementTree as ET
 import glob
 import numpy as np
 
+# enter how many sub-devisions you want in the x and the y direction
+# We will figure out the rest
+x_split = 1
+y_split = 4
 
-def img_center(image_path):
+
+def split_setup(image_path, x_split, y_split):
     image_obj = Image.open(image_path)
     img_width, img_height = image_obj.size
-    img_width_center = float(img_width)/2
-    img_height_center = float(img_height)/2
-    center = np.array([img_width_center, img_height_center])
-    return center
+    split_width = img_width/x_split
+    split_height = img_height/y_split
+    return split_width, split_height
 
 
-def crop(image_path, split, counter):
+def crop(image_path, i, j, split_width, split_height):
     # image_path: The path to the image to edit
     # coords: A tuple of x/y coordinates (x1, y1, x2, y2)
     # saved_location: Path to save the cropped image
     image_obj = Image.open(image_path)
+    split = np.array([0, 0, 0, 0])
+    split[0] = i * split_width
+    split[1] = j * split_height
+    split[2] = (i+1) * split_width
+    split[3] = (j+1) * split_height
     cropped_image = image_obj.crop(split)
-    cropped_image.save(str(counter) + image_path)
+    cropped_image.save('/Users/harrisonknoll/code/DarknetPrepScripts/test/' +
+                       str(i) + str(j) + "_" + image_path)
+    image_obj.close()
+    return split
 
 
-def crop_xml(image_path, split, counter):
+def crop_xml(image_path, split, i, j):
     xml_path = image_path.replace('.jpg', '.xml')
     tree = ET.parse(xml_path)
     root = tree.getroot()
-    for child in root:
-        if child.tag == "object":
-            for subchild in child:
-                if subchild.tag == "bndbox":
-                    x_min = float(subchild.find('xmin').text)
-                    y_min = float(subchild.find('ymin').text)
-                    x_max = float(subchild.find('xmax').text)
-                    y_max = float(subchild.find('ymax').text)
+    for child in root.findall('object'):
+        for subchild in child.findall('bndbox'):
+            x_min = float(subchild.find('xmin').text)
+            y_min = float(subchild.find('ymin').text)
+            x_max = float(subchild.find('xmax').text)
+            y_max = float(subchild.find('ymax').text)
 
-                    # redefine the bounding box wrt new top left
-                    x_min =
-                    Top_lef = np.array([x_min, y_min])
-                    Top_rig = np.array([x_max, y_min])
-                    bot_lef = np.array([x_min, y_max])
-                    bot_rig = np.array([x_max, y_max])
-                    center = np.array([x_center, y_center])
-                    # print(Top_lef)
+            if x_min < split[0]:
+                x_min = split[0]
+            if y_min < split[1]:
+                y_min = split[1]
+            if x_max > split[2]:
+                x_max = split[2]
+            if y_max > split[3]:
+                y_max = split[3]
 
-                    # Now lets express the for corners w.r.t. the center
-                    Top_lef = np.subtract(Top_lef, center)
-                    Top_rig = np.subtract(Top_rig, center)
-                    bot_lef = np.subtract(bot_lef, center)
-                    bot_rig = np.subtract(bot_rig, center)
-                    # print(Top_lef)
-                    # Define rotation matrix
-                    c, s = np.cos(np.deg2rad(deg)), np.sin(np.deg2rad(deg))
-                    rotate = np.array([[c, -s], [s, c]])
+            # if its out of bounds then get rid of it
+            if x_max < split[0] or \
+               y_max < split[1] or \
+               x_min > split[2] or \
+               y_min > split[3]:
+                root.remove(child)
+            else:
+                # Shift the coordinates to be with respect to zero zero
+                x_min = x_min - split[0]
+                y_min = y_min - split[1]
+                x_max = x_max - split[0]
+                y_max = y_max - split[1]
+                print(split)
 
-                    # Lets rotate some matrixes
-                    Top_lef = np.dot(rotate, np.transpose(Top_lef))
-                    Top_rig = np.dot(rotate, np.transpose(Top_rig))
-                    bot_lef = np.dot(rotate, np.transpose(bot_lef))
-                    bot_rig = np.dot(rotate, np.transpose(bot_rig))
+                subchild.find('xmin').text = str(x_min)
+                subchild.find('ymin').text = str(y_min)
+                subchild.find('xmax').text = str(x_max)
+                subchild.find('ymax').text = str(y_max)
 
-                    # Now we reference our new points back to the top left
-                    Top_lef = np.add(Top_lef, center)
-                    Top_rig = np.add(Top_rig, center)
-                    bot_lef = np.add(bot_lef, center)
-                    bot_rig = np.add(bot_rig, center)
 
-                    # Lets set all the x values in an array
-                    # Lets set all the y values in an array
-                    x_axis = np.array([Top_lef[0], Top_rig[0],
-                                       bot_lef[0], bot_rig[0]])
-                    y_axis = np.array([Top_lef[1], Top_rig[1],
-                                       bot_lef[1], bot_rig[1]])
 
-                    # Now we find the min and max of our X's and Y's
-                    max_x = np.amax(x_axis)
-                    min_x = np.amin(x_axis)
-                    max_y = np.amax(y_axis)
-                    min_y = np.amin(y_axis)
-                    print(min_x, min_y, max_x, max_y)
-                    rot_box = np.array([min_x, min_y, max_x, max_y])
-                    return rot_box
 
-                    # xml_filename = str(degree) + '_rotate_' + xml_path
-                    # tree.write(xml_filename)
+
+
+    xml_filename = str(i) + str(j) + '_' + xml_path
+    tree.write('/Users/harrisonknoll/code/DarknetPrepScripts/test/' +
+               xml_filename)
 
 
 if __name__ == '__main__':
 
-    rotate = 45
-
     for paths in glob.iglob('*.jpg'):
-        x_center, y_center = img_center(paths)
-        # rotate_img(paths, rotate)
-        # rotate_xml(paths, rotate, x_center, y_center)
-        rot_box = rotate_xml(paths, rotate, x_center, y_center)
-        rotate_img(paths, rotate, rot_box)
-
-        # print x_center, y_center
+        split_width, split_height = split_setup(paths, x_split, y_split)
+        i = 0
+        while i < x_split:
+            j = 0
+            while j < y_split:
+                split = crop(paths, i, j, split_width, split_height)
+                crop_xml(paths, split, i, j)
+                j += 1
+            i += 1
